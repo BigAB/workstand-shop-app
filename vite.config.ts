ddd6a91c2d0d1213a1a0013e425948a4fc4ee3c6
Adam Barrett <bigab@live.ca>
2025-03-28 14:33:11 -0600

    add config to start local pocketbase when you run vite dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 import Icons from 'unplugin-icons/vite';
 import mkcert from 'vite-plugin-mkcert';
+import { ChildProcess, spawn } from 'child_process';
 
 export default defineConfig({
 	plugins: [
@@ -10,12 +11,30 @@ export default defineConfig({
 			compiler: 'svelte',
 			autoInstall: true
 		}),
-		mkcert()
+		mkcert(),
+		{
+			name: 'start-local-pocketbase-server',
+			configureServer: (server) => {
+				let pocketbaseProcess: ChildProcess;
+
+				server.httpServer?.once('listening', () => {
+					console.log('Starting PocketBase...');
+					pocketbaseProcess = spawn('pocketbase', ['serve'], {
+						stdio: 'inherit',
+						shell: true
+					});
+				});
+
+				server.httpServer?.on('close', () => {
+					if (pocketbaseProcess) {
+						console.log('Stopping PocketBase...');
+						pocketbaseProcess.kill();
+					}
+				});
+			}
+		}
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		proxy: {}
 	}
 });
